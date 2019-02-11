6e0b529cb2f5ffa72a5882f8b30971ccc4eb2efd
jigsaw231 <jigsaw231@users.noreply.github.com>
2019-02-11 13:46:05 -0400

    Almost done

--- src/rdna.hpp
+++ src/rdna.hpp
@@ -1,18 +1,20 @@
 #include <iostream>
-#include <string>
 #include <random>
-using namespace std;
-string ranDNA (int seed, string bases, int n)
-{
-	std::random_device rd;
-	std::mt19937 eng(rd(seed));
 
-	
-	uniform_int_distrburtion<int> un{1,n};
-	for(i=0; i < n ; n++)
+using namespace std;
+string randDNA(int seed,string bases,int n)
+{
+	mt19937 eng1(seed);
+
+	int min = 0;
+	int max =bases.size()-1;
+	uniform_int_distribution<> uniform(min,max);
+	int index =0;
+	string dna="";
+	for(int i=0; i < n ; i++)
 	{
-		cout<<un(eng)<<" ";
-		bases = bases+bases;
+		index = uniform(eng1);
+		dna = dna + bases[index];
 	}	
-	return;
+	return dna;
 }
